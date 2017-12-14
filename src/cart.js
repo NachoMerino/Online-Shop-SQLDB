04a7891ec4faa7424f51b885e59d0ba5b93bfd5b
Leandro <leandro.frigerio@devugees.org>
2017-12-14 07:29:54 +0100

    cleaning, counting and adding quantity

--- src/cart.js
+++ src/cart.js
@@ -14,16 +14,22 @@ class Cart {
       localStorage.setItem('total', 1);
     } else {
       //  cart exists - retrive it and prepare to add
-      const total = parseInt(localStorage.getItem('total'), 10);
+      const total = Object.keys(this.cart.products).length;
       localStorage.setItem('total', total + 1);
       const storedProducts = JSON.parse(localStorage.getItem('cart'));
       this.cart.products = storedProducts;
+      //  check if the product is already in the cart
+      $.map(this.cart.products, ((storedProduct) => {
+        if (storedProduct.id === product.id) {
+          this.removeItem(product.id);
+          // eslint-disable-next-line no-param-reassign
+          product.quantity = storedProduct.quantity + 1;
+        }
+      }));
     }
     //  add product to cart
     this.cart.products.push(product);
     localStorage.setItem('cart', JSON.stringify(this.cart.products));
-    $('.badge').text(localStorage.getItem('total'));
-    $('.shopping-cart').show();
     return this.update();
   }
 
@@ -40,21 +46,25 @@ class Cart {
 
   update() {
     //  update badge and show/hide cart container
-    if (localStorage.getItem('cart') === null || localStorage.getItem('total') === '0') {
+    if (localStorage.getItem('cart') === null) {
       $('.badge').text(0);
       $('.badge').hide();
       $('.shopping-cart').hide();
       $('.cart').hide();
     } else {
-      $('.badge').text(localStorage.getItem('total'));
+      $('.badge').text(Object.keys(this.cart.products).length);
       $('.badge').show();
+      $('.shopping-cart').show();
       $('.cart').show();
       // updating items in cart
       $('.shopping-cart-items').empty();
-      const storedProducts = JSON.parse(localStorage.getItem('cart'));
+      const storedProducts = this.cart.products;
       let totalPrice = 0;
+      let totalQuantity = 0;
       storedProducts.forEach((product) => {
-        totalPrice += parseInt(product.price, 10);
+        totalQuantity += parseInt(product.quantity, 10);
+        totalPrice += parseInt(product.price, 10) * parseInt(product.quantity, 10);
+        $('.badge').text(product.quantity);
         $('.shopping-cart-items').append(`
           <li class="clearfix">
             <button type="button" class="close removeItemButton" aria-label="Close" data-id="${product.id}">
@@ -63,13 +73,13 @@ class Cart {
             <img class="cart-img" src="/static/assets/images/0${product.catid}.jpg" alt="${product.name}" />
             <span class="item-name">${product.name}</span>
             <span class="item-price">€ ${product.price}</span>
-            <span class="item-quantity">Quantity: 01</span>
+            <span class="item-quantity">Quantity: ${product.quantity}</span>
           </li>`);
         $('.total').text(`€ ${totalPrice}`);
+        $('.badge').text(`${totalQuantity}`);
       });
     }
     $('.removeItemButton').click((eventObj) => {
-      //  console.log('removing');
       //  define obj
       const { target } = eventObj;
       //  chrome / ff see different things!
